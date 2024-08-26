b1c085d0b4236468c1ae4b8bbd0ed8b2e8e74ece
Jitendra Adhikari <jiten.adhikary@gmail.com>
2024-08-26 10:01:06 +0700

    feat: expose compression ability

--- newconn.go
+++ newconn.go
@@ -8,3 +8,10 @@ import (
 func NewConn(conn net.Conn, isServer bool, readBufferSize, writeBufferSize int, writeBufferPool BufferPool, br *bufio.Reader, writeBuf []byte) *Conn {
 	return newConn(conn, isServer, readBufferSize, writeBufferSize, writeBufferPool, br, writeBuf)
 }
+
+func (c *Conn) SetupCompression() *Conn {
+	c.enableWriteCompression = true
+	c.newCompressionWriter = compressNoContextTakeover
+	c.newDecompressionReader = decompressNoContextTakeover
+	return c
+}
